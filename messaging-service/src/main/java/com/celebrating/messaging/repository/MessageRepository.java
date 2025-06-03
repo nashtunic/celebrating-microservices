097f4843cac198af5722862fbe6a57fa16d75a5c
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2025-06-03 22:59:32 +0300

    Update MessageRepository.java

--- messaging-service/src/main/java/com/celebrating/messaging/repository/MessageRepository.java
+++ messaging-service/src/main/java/com/celebrating/messaging/repository/MessageRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import java.time.LocalDateTime;
 
 public interface MessageRepository extends ReactiveCrudRepository<Message, Long> {
     
