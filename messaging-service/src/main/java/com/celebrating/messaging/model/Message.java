5188aa1f7dd33b2481e339d1dfb31a541ce8c4e5
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2025-06-03 22:57:24 +0300

    Update Message.java

--- messaging-service/src/main/java/com/celebrating/messaging/model/Message.java
+++ messaging-service/src/main/java/com/celebrating/messaging/model/Message.java
@@ -1,19 +1,24 @@
 package com.celebrating.messaging.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDateTime;
 
 @Data
+@Getter
+@Setter
 @Table("messages")
 public class Message {
     @Id
     private Long id;
+    private Long chatId;
     private Long senderId;
     private Long receiverId;
     private String content;
+    private boolean read;
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private boolean isRead;
+    private String mediaUrl;
 }
