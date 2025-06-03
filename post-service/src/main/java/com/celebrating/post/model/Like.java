c725707adab81a2909e03fc9248740ee6fb06311
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2025-06-03 22:48:59 +0300

    Update Like.java

--- post-service/src/main/java/com/celebrating/post/model/Like.java
+++ post-service/src/main/java/com/celebrating/post/model/Like.java
@@ -1,11 +1,15 @@
 package com.celebrating.post.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDateTime;
 
 @Data
+@Getter
+@Setter
 @Table("likes")
 public class Like {
     @Id
