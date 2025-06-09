44db857ef33ff8993a42765028537bfa27bc4c1b
nashtunic <38829580+nashtunic@users.noreply.github.com>
2025-06-09 15:19:39 +0300

    Add files via upload

---
+++ auth-service/src/main/java/com/celebrating/auth/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.celebrating.auth.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.celebrating.auth.repository")
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+} 
